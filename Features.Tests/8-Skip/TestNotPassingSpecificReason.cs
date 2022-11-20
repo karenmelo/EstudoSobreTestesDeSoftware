64afbae1ad2d72bdea8e849d6164ab095ad1a7b1
Karen Melo <karen_melo@live.com>
2022-11-20 17:43:08 -0300

    Incluida um exemplo de como escapar um teste caso o mesmo quebre apos melhorias, mudancas de versao

---
+++ Features.Tests/8-Skip/TestNotPassingSpecificReason.cs
@@ -0,0 +1,12 @@
+﻿namespace Features.Tests._8_Skip
+{
+    public class TestNotPassingSpecificReason
+    {
+        [Fact(DisplayName = "New Client 2.0", Skip = "New version 2.0 breaking")]
+        [Trait("Category", "Skip the Tests")]
+        public void Test_NotPassing_NewVersionNotCompatible()
+        {
+            Assert.True(false);
+        }
+    }
+}
