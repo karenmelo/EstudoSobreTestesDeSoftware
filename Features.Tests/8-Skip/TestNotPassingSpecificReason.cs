f25e4ebcfc167233d09a4047bf67643f712b2fd9
Karen Melo <karen_melo@live.com>
2022-11-25 00:19:11 -0300

    Inclusao da classe de teste de cpf e uma de validacao de cpf para realizacao de teste e verificar o code coverage(cobertura de testes de codigo), lembrando que a ferramenta 'Analyze Code Coverage for All Tests' so esta disponivel na versao Enterprise do VS, uma ferramenta gratuita e o openCover

--- Features.Tests/8-Skip/TestNotPassingSpecificReason.cs
+++ Features.Tests/8-Skip/TestNotPassingSpecificReason.cs
@@ -1,8 +1,8 @@
-﻿namespace Features.Tests._8_Skip
+﻿namespace Features.Tests
 {
     public class TestNotPassingSpecificReason
     {
-        [Fact(DisplayName = "New Client 2.0", Skip = "New version 2.0 breaking")]
+        [Fact(DisplayName = "New Client 2.0", Skip = "New version 2.0 breaking")] 
         [Trait("Category", "Skip the Tests")]
         public void Test_NotPassing_NewVersionNotCompatible()
         {
---
+++ Features.Tests/9-Code Coverage/CpfValidationTests.cs
@@ -0,0 +1,40 @@
+﻿using Features.Core;
+using FluentAssertions;
+
+namespace Features.Tests
+{
+    public class CpfValidationTests
+    {
+        [Theory(DisplayName = "CPF Valid")]
+        [Trait("Category", "CPF Validation Tests")]
+        [InlineData("56245650038")]
+        [InlineData("67857110027")]
+        [InlineData("26197024020")]
+        [InlineData("01489184090")]
+        [InlineData("13830803800")]
+        public void Cpf_ValidateMultipleNumbers_AllMustBeValid(string cpf)
+        {
+            //Arrange
+            var cpfValidation = new CpfValidation();
+
+            //Act & Assert
+            cpfValidation.IsValid(cpf).Should().BeTrue();
+        }
+
+        [Theory(DisplayName = "CPF Valid")]
+        [Trait("Category", "CPF Validation Tests")]
+        [InlineData("5624565003811")]
+        [InlineData("6785711002711")]
+        [InlineData("26197024d02023")]
+        [InlineData("014891840d90455")]
+        public void Cpf_ValidateMultipleNumbers_AllMustBeInvalid(string cpf)
+        {
+            //Arrange
+            var cpfValidation = new CpfValidation();
+
+            //Act & Assert
+            cpfValidation.IsValid(cpf).Should().BeFalse();
+        }
+
+    }
+}
